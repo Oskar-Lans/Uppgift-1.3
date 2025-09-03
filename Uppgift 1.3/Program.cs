7334e207c0cfc0b84e266fd0454f2667ccd79426
osla08001 <osla08001@E-PW04SMBR>
2025-09-03 12:59:11 +0200

    Add project files.

---
+++ Uppgift 1.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Övningsupgift_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(" ____________________________________________________________ ");
+            Console.WriteLine("|       __                                                   |");
+            Console.WriteLine("|      |  |                                                  |");
+            Console.WriteLine("|     _|__|_                                                 |");
+            Console.WriteLine("| __    /\\    __    __________________________________       |");
+            Console.WriteLine("|  |   /  \\   |    |\"Jag är en triangel med en twist!\"|      |");
+            Console.WriteLine("|  |  /    \\  |   /| \"Men jag har även en hatt!\"      |      |");
+            Console.WriteLine("|   \\/  0 0 \\/   / |__________________________________|      |");
+            Console.WriteLine("|   /    o   \\  /                                            |");
+            Console.WriteLine("|  /__________\\                                              |");
+            Console.WriteLine("|     |    |                                                 |");
+            Console.WriteLine("|     |    |                                                 |");
+            Console.WriteLine("|    _|    |_                                                |");
+            Console.WriteLine("|____________________________________________________________| ");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
